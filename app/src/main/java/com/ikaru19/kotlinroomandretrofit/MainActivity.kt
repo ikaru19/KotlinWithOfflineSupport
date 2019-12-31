4d42d2e1ad2a2efaf52686dd67861523e0f3dc79
ikaru19 <syafrizalizal17@gmail.com>
2019-12-31 12:46:19 +0700

    change from GAA to repository system

--- app/src/main/java/com/ikaru19/kotlinroomandretrofit/MainActivity.kt
+++ app/src/main/java/com/ikaru19/kotlinroomandretrofit/MainActivity.kt
@@ -12,87 +12,25 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    private var postDatabase : PostRoomDatabase? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        postDatabase = PostRoomDatabase.getDatabase(this)!!
+        var myrepo = PostRepository(this)
+        myrepo.getPostFromInternet()
+        var posts = myrepo.GetDataFromDB().execute().get()
 
 
-        // get post data
-        val postServices = DataRepository.create()
-        postServices.getPosts().enqueue(object : Callback<List<PostModel>> {
-
-            override fun onResponse(call: Call<List<PostModel>>, response: Response<List<PostModel>>) {
-
-
-                if (response.isSuccessful) {
-                    ClearAllData(this@MainActivity).execute()
-                    Toast.makeText(this@MainActivity,"From Internet",Toast.LENGTH_LONG).show()
-                    val data = response.body()
-                    tv_hasil.setText("")
-                    data?.map {
-                        Log.d("MYRETRO", "datanya ${it.body}")
-                        tv_hasil.append("datanya ${it.id}" + "\n")
-                        InsertTask(this@MainActivity,it).execute()
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<PostModel>>, error: Throwable) {
-                Log.e("MYRETRO", "errornya ${error.message}")
-                Toast.makeText(this@MainActivity,"No Internet trying get data offline",Toast.LENGTH_LONG).show()
-                GetDataFromDB(this@MainActivity).execute()
-
-            }
-        })
-    }
-
-    private class InsertTask(var context: MainActivity,var postModel: PostModel): AsyncTask<Void, Void, Boolean>() {
-        override fun doInBackground(vararg params: Void?): Boolean {
-            context.postDatabase!!.postDao().insert(postModel)
-            return true
-        }
-
-        override fun onPostExecute(result: Boolean?) {
-            if (result!!){
-                Toast.makeText(context,"Added to database",Toast.LENGTH_SHORT).show()
-            }
-        }
-
-    }
-
-    private class GetDataFromDB(var context: MainActivity) : AsyncTask<Void,Void,List<PostModel>>(){
-        override fun doInBackground(vararg params: Void?): List<PostModel> {
-            return context.postDatabase!!.postDao().getAllPosts()
-        }
-
-        override fun onPostExecute(result: List<PostModel>?) {
-            if (result!!.size > 0){
-                Toast.makeText(context,"From Local DB",Toast.LENGTH_SHORT).show()
-                context.tv_hasil.setText("")
-                for (i in 0..result.size - 1){
-                    context.tv_hasil.append(result[i].id.toString() + "\n")
+            if (posts!!.size > 0){
+                Toast.makeText(this,"From Local DB",Toast.LENGTH_SHORT).show()
+                tv_hasil.setText("")
+                for (i in 0..posts.size - 1){
+                   tv_hasil.append(posts[i].id.toString() + "\n")
                 }
             }else{
-                Toast.makeText(context,"You need to connect internet first",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"You need to connect internet first",Toast.LENGTH_SHORT).show()
             }
-        }
 
     }
 
-    private class ClearAllData(var context: MainActivity) : AsyncTask<Void,Void,Boolean>(){
-        override fun doInBackground(vararg params: Void?): Boolean {
-            context.postDatabase!!.postDao().deleteAll()
-                   return true
-        }
-
-        override fun onPostExecute(result: Boolean?) {
-            if (result!!){
-                Toast.makeText(context,"Clear Database",Toast.LENGTH_SHORT).show()
-            }
-        }
-
-    }
 }
---
+++ app/src/main/java/com/ikaru19/kotlinroomandretrofit/PostRepository.kt
@@ -0,0 +1,143 @@
+package com.ikaru19.kotlinroomandretrofit
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import android.content.Intent
+
+
+
+
+
+
+
+
+
+
+
+class PostRepository(context: Context){
+    var context : Context
+    var postDatabase : PostRoomDatabase? = null
+    var posts : List<PostModel>? = null
+    val postServices = DataRepository.create()
+
+
+    init {
+        this.context = context
+        postDatabase = PostRoomDatabase.getDatabase(context)!!
+    }
+
+
+    fun getPostFromInternet(){
+        postServices.getPosts().enqueue(object : Callback<List<PostModel>> {
+            override fun onResponse(call: Call<List<PostModel>>, response: Response<List<PostModel>>) {
+                if (response.isSuccessful) {
+                    ClearAllData().execute()
+                    Toast.makeText(context,"From Internet", Toast.LENGTH_LONG).show()
+                    print("ambil internet")
+                    val data = response.body()
+                    data?.map {
+                        InsertTask(it).execute()
+                    }
+                    showDialog()
+                }
+            }
+
+            override fun onFailure(call: Call<List<PostModel>>, error: Throwable) {
+                Log.e("MYRETRO", "errornya ${error.message}")
+                Toast.makeText(context,"No Internet trying get data offline", Toast.LENGTH_LONG).show()
+
+            }
+        })
+    }
+
+
+    private inner class InsertTask(var postModel: PostModel): AsyncTask<Void, Void, Boolean>() {
+        override fun doInBackground(vararg params: Void?): Boolean {
+            postDatabase!!.postDao().insert(postModel)
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean?) {
+            if (result!!){
+                Toast.makeText(context,"Added to database",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    inner class GetDataFromDB : AsyncTask<Void, Void, List<PostModel>>(){
+        override fun doInBackground(vararg params: Void?): List<PostModel> {
+            posts = postDatabase!!.postDao().getAllPosts()
+            return postDatabase!!.postDao().getAllPosts()
+        }
+
+        override fun onPostExecute(result: List<PostModel>?) {
+            if (result!!.size > 0){
+
+                posts = result
+            }else{
+            Toast.makeText(context,"You need to connect internet first",Toast.LENGTH_SHORT).show()
+        }
+        }
+    }
+
+    private inner class ClearAllData : AsyncTask<Void, Void, Boolean>(){
+        override fun doInBackground(vararg params: Void?): Boolean {
+            postDatabase!!.postDao().deleteAll()
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean?) {
+            if (result!!){
+                Toast.makeText(context,"Clear Database",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun showDialog(){
+            val builder = AlertDialog.Builder(context)
+
+            // Set the alert dialog title
+            builder.setTitle("Update Data")
+
+            // Display a message on alert dialog
+            builder.setMessage("Update your data and restart ?")
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("YES"){dialog, which ->
+                // Do something when user press the positive button
+                Toast.makeText(context,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+                (context as Activity).finish()
+                context.startActivity(Intent(context, MainActivity::class.java))
+                (context as Activity).finishAffinity()
+                // Change the app background color
+
+            }
+
+
+            // Display a negative button on alert dialog
+            builder.setNegativeButton("No"){dialog,which ->
+                Toast.makeText(context,"You are not update your application",Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
+
+    }
+
+
+
+}
